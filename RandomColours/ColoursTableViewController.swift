9312cc51a6e4c6c4418f427909a38fefb41bdeae
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-01 17:00:17 +0100

    Random colours generated and displayed on home screen

--- RandomColours/ColoursTableViewController.swift
+++ RandomColours/ColoursTableViewController.swift
@@ -8,25 +8,43 @@
 import UIKit
 
 class ColoursTableViewController: UIViewController {
-
+    var colours: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initialiseRandomColours()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initialiseRandomColours() {
+        for _ in 1...50 {
+            colours.append(createRandomColour())
+        }
     }
-    */
+    
+    func createRandomColour() -> UIColor {
+        return UIColor(red: generateRandomCGFloat(), green: generateRandomCGFloat(), blue: generateRandomCGFloat(), alpha: 1)
+    }
+    
+    func generateRandomCGFloat() -> CGFloat {
+        return CGFloat.random(in: 0...1)
+    }
+}
 
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
+extension ColoursTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColourCell") else {
+            return UITableViewCell()
+        }
+        
+        cell.backgroundColor = colours[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColoursDetailVC", sender: nil)
     }
 }
