df24a755379d2ece95b73b797eef6a4f61353f75
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-01 12:29:16 +0100

    Initial Commit

---
+++ RandomColours/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColours
+//
+//  Created by Ryan Henzell-Hill on 01/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
